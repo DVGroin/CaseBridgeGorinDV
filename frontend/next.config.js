c1fc7dc7f6b782df0550dbabfd97e3746dd568e8
DVGorin <gorin.damirr@gmail.com>
2026-04-30 16:02:51 +0300

    Update next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // если бэкенд на другом порту, можно настроить прокси
+  // async rewrites() { ... }
+};
+
+module.exports = nextConfig;
